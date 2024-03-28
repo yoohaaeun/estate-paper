29952fda30e3b92ad648b5c4aa6d23c98d9e1778
yoohaaeun <yoohaaeun@gmail.com>
2024-03-28 16:39:45 +0900

    feat: Area 컴포넌트 작업

--- src/components/console/Area.tsx
+++ src/components/console/Area.tsx
@@ -1,7 +1,74 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { useProperty } from '../../context/PropertyContext';
+import { Title } from '../CommonStyles';
+import { aptData, IArea } from '../../aptData';
+import CustomApt from './CustomApt';
 
 export default function Area() {
-  return <Wrapper>Area</Wrapper>;
+  const { property, setProperty } = useProperty();
+
+  const selectedApt = aptData.find((apt) => apt.name === property.apt);
+
+  const handleAreaClick = (selectedArea: IArea) => {
+    const { area, supply_area, exclusive_area } = selectedArea;
+
+    setProperty({
+      ...property,
+      area,
+      supplyArea: supply_area,
+      exclusiveArea: exclusive_area,
+    });
+  };
+
+  return (
+    <div>
+      {!property.custom && (
+        <>
+          <Title>면적</Title>
+          <Buttons>
+            {selectedApt?.areas.map((area) => (
+              <Button
+                key={area.area}
+                onClick={() => handleAreaClick(area)}
+                selected={property.area === area.area}
+              >
+                {area.area}
+              </Button>
+            ))}
+          </Buttons>
+        </>
+      )}
+
+      {property.custom && <CustomApt />}
+    </div>
+  );
 }
 
-const Wrapper = styled.div``;
+const Buttons = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+`;
+
+const Button = styled.button<{ selected: boolean }>`
+  width: 90px;
+  height: 90px;
+  border: 2px solid #c5c5c5;
+  border-radius: 20px;
+  font-size: 23px;
+  font-weight: 600;
+  color: #4b495c;
+  background: #f6f7fb;
+
+  ${(props) =>
+    props.selected &&
+    css`
+      background: #fceec9;
+      box-shadow: 0px 5px 4px rgba(0, 0, 0, 0.1);
+    `}
+
+  &:hover {
+    background: #fceec9;
+    box-shadow: 0px 5px 4px rgba(0, 0, 0, 0.1);
+  }
+`;
