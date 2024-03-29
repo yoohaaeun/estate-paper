1b3d08fe75409f1d5ebfff9cbc4c8334a1ab20d8
yoohaaeun <yoohaaeun@gmail.com>
2024-03-29 11:22:17 +0900

    fix: 커스텀 속성에 따른 면적 정보 표시 수정

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -9,10 +9,17 @@ const Preview = forwardRef<HTMLDivElement>((props, ref) => {
     <Wrapper ref={ref}>
       <Name>{property.apt}</Name>
       <Type type={property.type}>{property.type}</Type>
-      <Area>
-        <span>공급 {property.supplyArea}㎡</span>
-        <span>전용 {property.exclusiveArea}㎡</span>
-      </Area>
+      {property.custom ? (
+        <Area>
+          <span>{property.supplyArea}</span>
+          <span>{property.exclusiveArea}</span>
+        </Area>
+      ) : (
+        <Area>
+          <span>공급 {property.supplyArea}㎡</span>
+          <span>전용 {property.exclusiveArea}㎡</span>
+        </Area>
+      )}
       <Description>{property.desc}</Description>
       <Price>{property.price}</Price>
       <Banner>
